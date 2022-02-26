b3e1b229af5382d5fba79fd7d7879eab10f09a88
hina <hina@hinaloe.net>
2022-02-27 03:52:15 +0900

    eslintrc standarded

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "./tsconfig.json",
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        'react/prop-types': 'off',
-    },
-    "ignorePatterns": [
-        ".eslintrc.js",
-        "vite.config.ts",
-    ],
-    "settings": {
-        "react": {
-            "version": "detect"
-        },
-    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'react/prop-types': 'off'
+  },
+  ignorePatterns: [
+    '.eslintrc.js',
+    'vite.config.ts'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 }
